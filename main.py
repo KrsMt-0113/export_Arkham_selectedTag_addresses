afa632c7e435567ce86e751c8f7c7588d9d82d34
KrsMt. <2021865415@qq.com>
2025-09-04 21:40:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import requests
 import csv
 
 page = 1
-# fill your api key here
+
 headers = {
     "API-Key": "YOUR_API_KEY"
 }
-# fill the tag after 'tag=' ⬇️
-url = "https://api.arkm.com/tag/top?tag=YOUR_TAG&page="
 
-total = requests.get("https://api.arkm.com/tag/YOUR_TAG/count_addresses", headers= headers).json()
+url = "https://api.arkm.com/tag/top?tag=YOUR_selectedTag&page="
+
+result = {}
+
+total = requests.get("https://api.arkm.com/tag/cex/count_addresses", headers= headers).json()
 print("Total addresses to fetch:", total)
-
-result = []
 
 while True:
     print("Fetching page:", page)
@@ -20,7 +20,8 @@ while True:
         response = requests.get(url+str(page), headers= headers, timeout= 100)
         data = response.json().get("addresses")
         for i in data:
-            temp_result = {
+            key = i.get("address")
+            result[key] = {
                 "address": i.get("address"),
                 "entityName": i.get("arkhamEntity").get("name"),
                 "entityId": i.get("arkhamEntity").get("id"),
@@ -28,10 +29,9 @@ while True:
                 "isContract": i.get("contract")
             }
             if i.get("arkhamLabel"):
-                temp_result["label"] = i.get("arkhamLabel").get("name")
+                result[key]["label"] = i.get("arkhamLabel").get("name")
             else:
-                temp_result["label"] = "N/A"
-            result.append(temp_result)
+                result[key]["label"] = "N/A"
         print(f"page {page} done, total addresses fetched: {len(result)}")
         page += 1
     except Exception as e:
@@ -41,7 +41,7 @@ while True:
         break
 
 with open("cex_addresses.csv", "w", encoding="utf-8") as f:
-    writer = csv.writer(f)
-    writer.writerow(["address", "entityName", "entityId", "chain", "label", "isContract"])
-    for address in result:
-        writer.writerow([address["address"], address["entityName"], address["entityId"], address["chain"], address["label"], address["isContract"]])
+    fieldnames = ["address", "entityName", "entityId", "chain", "label", "isContract"]
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(result)
