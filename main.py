d5fd48edd4442e315ddc12f41d3cb066a96c64d7
KrsMt. <2021865415@qq.com>
2025-09-04 18:11:20 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+import csv
+
+page = 1
+# fill your api key here
+headers = {
+    "API-Key": "YOUR_API_KEY"
+}
+# fill the tag after 'tag=' ⬇️
+url = "https://api.arkm.com/tag/top?tag=&page="
+
+result = []
+
+while True:
+    print("Fetching page:", page)
+    try:
+        response = requests.get(url+str(page), headers= headers, timeout= 100)
+        data = response.json().get("addresses")
+        for i in data:
+            temp_result = {
+                "address": i.get("address"),
+                "entityName": i.get("arkhamEntity").get("name"),
+                "entityId": i.get("arkhamEntity").get("id"),
+                "chain": i.get("chain"),
+                "isContract": i.get("contract")
+            }
+            if i.get("arkhamLabel"):
+                temp_result["label"] = i.get("arkhamLabel").get("name")
+            else:
+                temp_result["label"] = "N/A"
+            result.append(temp_result)
+        print(f"page {page} done, total addresses fetched: {len(result)}")
+        page += 1
+    except Exception as e:
+        print(f"Error fetching page {page}: {e}")
+        continue
+    if response.json().get("allAddressesFetched"):
+        break
+
+with open("cex_addresses.csv", "w", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["address", "entityName", "entityId", "chain", "label", "isContract"])
+    for address in result:
+        writer.writerow([address["address"], address["entityName"], address["entityId"], address["chain"], address["label"], address["isContract"]])
